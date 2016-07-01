0c016bd91339c651099e8afb1a9b3fd44387dfd7
notorious.no@gmail.com <notorious.no@gmail.com>
2016-06-30 23:32:06 -0400

    added interrupted deferred

---
+++ src/interrupted.py
@@ -0,0 +1,24 @@
+from klein import Klein
+from twisted.internet import defer, reactor
+
+
+app = Klein()
+
+
+@app.route('/interrupted')
+def interruptRoute(request):
+    d = defer.Deferred()
+    d.addCallback(_delayedRender)
+    reactor.callLater(10, d.callback, request)
+    request.notifyFinish().addErrback(_responseFailed, d)
+    return d
+
+def _responseFailed(error, call):
+    print('Cancelled!')
+    call.cancel()
+
+def _delayedRender(request):
+    request.write('Sorry for the delay'.encode('utf-8'))
+
+
+app.run('localhost', 9000)
